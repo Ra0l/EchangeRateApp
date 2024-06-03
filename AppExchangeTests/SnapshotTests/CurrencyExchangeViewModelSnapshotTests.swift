2d2a4fe8a24c33435e749eec557a4c772d9cf1be
Raul Kevin Aliaga Shapiama <45242019+Ra0l@users.noreply.github.com>
2024-06-02 21:56:25 -0500

    test: add snapshot test

---
+++ AppExchangeTests/SnapshotTests/CurrencyExchangeViewModelSnapshotTests.swift
@@ -0,0 +1,19 @@
+//
+//  CurrencyExchangeViewModelSnapshotTests.swift
+//  AppExchangeTests
+//
+//  Created by Raul Kevin Aliaga Shapiama on 6/2/24.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import AppExchange
+
+class CurrencyExchangeViewModelSnapshotTests: XCTestCase {
+    
+    var mockService: MockExchangeRateService!
+    func testFetchCurrencies() {
+        let fetchCurrency  = ExchangeRateView(viewModel: .init(service: mockService!))
+        assertSnapshot(of: fetchCurrency, as: .image)
+    }
+}
